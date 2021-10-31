da88fdd28eb51db90e2fe8c5c9dd29c69cf7d151
souldrop <imneocoder@gmail.com>
2021-10-31 22:44:00 +0530

    Finished Gatsby Tutorial Part 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,39 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
-const IndexPage = () => {
+const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="tiny-steps"
-        src="../images/tiny-steps.jpg"
-      />
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))
+      }
     </Layout>
   )
 }
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`
+
+export default BlogPage
